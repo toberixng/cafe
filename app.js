d326aae5f999f2111c2c2971b481cc8f8d4dbe7b
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-11-20 15:26:21 +0100

    feat: adding item

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
+const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
 
+// create element & render cafe
+function renderCafe(doc) {
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let city = document.createElement('span');
 
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    city.textContent = doc.data().city;
+
+    li.appendChild(name);
+    li.appendChild(city);
+
+    cafeList.appendChild(li);
+}
+
+// getting data
+db.collection('cafes').get().then(snapshot => {
+    snapshot.docs.forEach(doc => {
+        renderCafe(doc);
+    });
+});
+
+// adding data
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    });
+
+    form.name.value = ''
+    form.city.value = ''
+})
 
