8d8f7230a75b279682d6ab98e91bea452e4690ba
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-11-25 09:17:26 +0100

    feat: real time capability

--- app.js
+++ app.js
@@ -5,15 +5,18 @@ const form = document.querySelector('#add-cafe-form');
 function renderCafe(doc) {
     let li = document.createElement('li');
     let name = document.createElement('span');
+    let last = document.createElement('span');
     let city = document.createElement('span');
     let cross = document.createElement('div');
 
     li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name;
+    last.textContent = doc.data().last;
     city.textContent = doc.data().city;
     cross.textContent = 'x';
 
     li.appendChild(name);
+    li.appendChild(last);
     li.appendChild(city);
     li.appendChild(cross);
 
@@ -29,19 +32,25 @@ cross.addEventListener('click', e => {
 }
 
 // getting data
-db.collection('cafes').orderBy('city').get().then(snapshot => {
-    snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-    });
-});
+// db.collection('cafes').where('city', '==', 'Lagos').orderBy('name').get().then(snapshot => {
+//     snapshot.docs.forEach(doc => {
+//         renderCafe(doc);
+//     });
+// });
+
+
 
 // saving data
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     db.collection('cafes').add({
         name: form.name.value,
+        last: form.last.value,
         city: form.city.value
     });
     form.name.value = '';
     form.city.value = '';
+    form.last.value = '';
 });
+
+// real time listener
